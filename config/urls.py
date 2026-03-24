6c7b4fc9cd6c7120c6d8778558a225bf674e65ca
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-03-24 18:21:51 +0500

    config added

---
+++ config/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # API sxema (JSON formatda)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # ReDoc UI
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Domenlar API lari
+    path('api/v1/pages/', include('domains.pages.api.urls')),
+    path('api/v1/news/', include('domains.news.api.urls')),
+]

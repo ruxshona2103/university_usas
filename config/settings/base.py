b21f8b426b56b98cd33f9fa27144f4eb2d769532
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-03-25 18:02:57 +0500

    updated

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-c4=8+h3!nl)l-!g*=so#ren#i7ap5880+htec&_hwb4mhskj)8')
 
-DEBUG = False 
+DEBUG = True 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 

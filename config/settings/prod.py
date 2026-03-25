72ea2d282f134b4a398ad815b37383c162440578
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-03-25 18:08:14 +0500

    updated

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,8 +12,9 @@ ALLOWED_HOSTS = [
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
+        ssl_require=True 
     )
 }
 

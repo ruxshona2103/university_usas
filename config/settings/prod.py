9705a5af476f34cc140de121078c88d2966e6d93
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-03-27 11:00:14 +0500

    imagekit added to cloud storage

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,7 @@
 from .base import *
 import dj_database_url
 import os
+from django.core.exceptions import ImproperlyConfigured
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
@@ -10,6 +11,7 @@ ALLOWED_HOSTS = [
     if host.strip()
 ]
 
+# postgresql manzili
 database_url = os.getenv('DATABASE_URL')
 if database_url:
     DATABASES = {
@@ -27,4 +29,31 @@ else:
         }
     }
 
+# middleware sozlamasi
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+
+# storage sozlamasi 
+imagekit_public_key = os.getenv('IMAGEKIT_PUBLIC_KEY')
+imagekit_private_key = os.getenv('IMAGEKIT_PRIVATE_KEY')
+imagekit_url_endpoint = os.getenv('IMAGEKIT_URL_ENDPOINT')
+
+if imagekit_public_key and imagekit_private_key and imagekit_url_endpoint:
+    INSTALLED_APPS += ['imagekitio_storage']
+    IMAGEKIT_STORAGE = {
+        'PUBLIC_KEY': imagekit_public_key,
+        'PRIVATE_KEY': imagekit_private_key,
+        'URL_ENDPOINT': imagekit_url_endpoint,
+    }
+    STORAGES = {
+        "default": {
+            "BACKEND": "imagekitio_storage.storage.ImageKitRenderStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+else:
+    raise ImproperlyConfigured(
+        'Production uchun IMAGEKIT_PUBLIC_KEY, IMAGEKIT_PRIVATE_KEY va IMAGEKIT_URL_ENDPOINT env qiymatlari majburiy.'
+    )

30261c2ab3caaa9c3043305e4b2a2478a08641c5
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-03-27 10:59:57 +0500

    imagekit added to cloud storage

--- config/settings/base.py
+++ config/settings/base.py
@@ -116,15 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Django 5.x da STATICFILES_STORAGE o'rniga STORAGES ishlatiladi
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

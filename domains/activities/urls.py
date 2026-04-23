5b5e013f248228ef55742f8bf27da723588d76bb
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-04-23 16:07:11 +0500

    id uuid updated

--- domains/activities/urls.py
+++ domains/activities/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('activities/contract-prices/', ContractPriceListAPIView.as_view(),    name='contract-prices'),
     path('activities/vehicles/',        ServiceVehicleListAPIView.as_view(),   name='service-vehicles'),
     path('activities/oquv-faoliyat/',  IlmiyFaoliyatListAPIView.as_view(),   name='oquv-faoliyat-list'),
-    path('activities/oquv-faoliyat/<int:pk>/', IlmiyFaoliyatDetailAPIView.as_view(), name='oquv-faoliyat-detail'),
+    path('activities/oquv-faoliyat/<uuid:pk>/', IlmiyFaoliyatDetailAPIView.as_view(), name='oquv-faoliyat-detail'),
 ]
